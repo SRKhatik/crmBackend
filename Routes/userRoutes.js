ac6cf370c4d6a428561aa8b4a8a94d67f0eb6f60
unknown <khatikshubham1995@gmail.com>
2023-06-02 13:22:51 +0530

    delete user

--- Routes/userRoutes.js
+++ Routes/userRoutes.js
@@ -1,13 +1,20 @@
+const {
+  createUser,
+  getAllUsers,
+  getUserById,
+  updateUser,
+  deleteUser,
+} = require("../Controllers/userController");
+const {
+  verifyToken,
+  isAdmin,
+  isAdminOrOwnUser,
+} = require("../Middlewares/authJWT");
 
-const {createUser, getAllUsers, getUserById, updateUser,deleteUser} = require("../Controllers/userController");
-const {verifyToken, isAdmin, isAdminOrOwnUser} = require("../Middlewares/authJWT");
-
-module.exports = function(app){
-
-    app.post("/crm/api/v1/users",createUser);
-    app.get("/crm/api/v1/users",[verifyToken, isAdmin],getAllUsers);
-    app.get("/crm/api/v1/users/:id",[verifyToken,isAdminOrOwnUser],getUserById);
-    app.put("/crm/api/v1/users/:id",[verifyToken, isAdmin],updateUser);
-    app.delete("/crm/api/vi/users/:id"[verifyToken, isAdmin],deleteUser)
-
-}
+module.exports = function (app) {
+  app.post("/crm/api/v1/users", createUser);
+  app.get("/crm/api/v1/users", [verifyToken, isAdmin], getAllUsers);
+  app.get("/crm/api/v1/users/:id",[verifyToken, isAdminOrOwnUser],getUserById);
+  app.put("/crm/api/v1/users/:id", [verifyToken, isAdmin], updateUser);
+  app.post("/crm/api/vi/users/:id",[(verifyToken, isAdmin)], deleteUser);
+};
